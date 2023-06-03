bbc126fa7c58f59517953bb6db336e875a843157
Blend Berisha <beny343434@gmail.com>
2023-06-03 20:20:57 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Footer,Blog,Possibility,Features,WhatGP3,Header} from './containers';
+import {Navbar,Brand,CTA} from "./components"
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+<Navbar/>
+<Header/> 
+      </div>
+      <Brand/>
+      <WhatGP3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
